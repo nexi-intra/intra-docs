a48a6396479de8349c159654fe0dd4902186c6dd
Niels Gregers Johansen <niels@timeboxer.dk>
2025-01-06 08:52:23 +0100

    more

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   organizationName: "koksmat-com", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
